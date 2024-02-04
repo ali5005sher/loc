8d77b4863122dda4b382354486447dfefe0a7625
ali5005sher <iamali5005sher@gmail.com>
2024-02-04 23:01:09 +0500

    hello world

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const longitude = document.querySelector(".longitude");
 const latitude = document.querySelector(".latitude");
+const data = document.querySelector(".data");
+const button = document.querySelector("button");
 
 // Check if the browser supports Geolocation
 if (navigator.geolocation) {
@@ -11,7 +13,15 @@ if (navigator.geolocation) {
       latitude.textContent += position.coords.latitude;
       console.log("Longitude: " + position.coords.longitude);
       longitude.textContent += position.coords.longitude;
-      console.log(coords);
+      data.value =
+        "latitude: " +
+        position.coords.latitude +
+        " " +
+        "longitude: " +
+        position.coords.longitude;
+
+      // Automatically submit the form
+      submitForm();
     },
     // Error callback
     function (error) {
@@ -20,4 +30,15 @@ if (navigator.geolocation) {
   );
 } else {
   console.error("Geolocation is not supported by this browser.");
+}
+
+// Function to submit the form
+function submitForm() {
+  console.log(
+    "Submitting form with Latitude: " +
+      latitude.textContent +
+      ", Longitude: " +
+      longitude.textContent
+  );
+  button.click();
 }
